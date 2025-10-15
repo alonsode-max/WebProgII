e47d4561f35e973da406f48eba1a63a6780405c4
Alonso <alonso.de@alumni.immune.institute>
2025-10-15 16:27:44 +0200

    Añadido plantilla

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import "../css/App.css"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
