38365a7f7e5345668a8cdd184442498b7dbc501a
Alonso <alonso.de@alumni.immune.institute>
2025-10-15 16:38:57 +0200

    Añadido esqueleto del header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+      <Header/>
       <Routes>
         <Route></Route>
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <nav>
+        <ul className="head">
+            <li>Home</li>
+            <li>Perfil</li>
+            <li>Admin</li>
+            <li>Misiones</li>
+            <li>Registrarse</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
