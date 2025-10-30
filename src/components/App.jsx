e6eb8f34ba0153bca1f47374f8f2fbef584934b6
ivanroqueceb <ivan.roque.ceb@immune.institute>
2025-10-30 18:09:49 +0100

    cambios de front

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
-import { Route, Routes } from "react-router-dom"
-import "../css/App.css"
+import Home from "./Home";
+import Login from "./Login";
+import Admin from "./Admin";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import MissionList from "./MissionList";
+
 
 function App() {
+  const [user, setUser] = useState(null); 
 
   return (
     <>
-      <Routes>
-        <Route></Route>
-      </Routes>
+     <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/missions" element={<MissionList/>}/>
+         <Route path="/login" element={<Login setUser={setUser} />} />
+         <Route path="/admin" element={<Admin user={user} />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/contact" element={<Contact />} />
+       </Routes>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
